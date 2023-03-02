bba2703df657cbec3d3ad0cc24fbc308820c20eb
ellsouza <ellensoyza007@gmail.com>
2023-03-01 22:13:06 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main
+{	public static void main(String[] args) {
+    
+//Cria uma variáveel do tipo long chamada valor e insere o número 50 na memória    
+long value = 505645455650L;
+double batata = 10.99;
+float morango = 9.33f;
+boolean casado = true;
+String num1 = "Número 1";
+//Buscar o valor na memória e mostra na saída/terminal
+//obs: apenas digitar println+TAB p/ atalho System
+System.out.println("O número que está armazenado na memória é: " + value);
+System.out.println("Batata: " + batata);
+System.out.println("Morango: " + morango);
+System.out.println("Casado? " + casado);
+System.out.println(num1);
+	}
+}
